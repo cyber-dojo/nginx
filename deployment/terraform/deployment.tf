cf3c318822805e68cd8b709e716395cbbcd0a656
zhelezovartem <zhelezovartem@gmail.com>
2024-10-28 22:31:31 +0300

    enable container restart policy for beta

--- deployment/terraform/deployment.tf
+++ deployment/terraform/deployment.tf
@@ -1,16 +1,18 @@
 module "ecs-service" {
-  source                    = "s3::https://s3-eu-central-1.amazonaws.com/terraform-modules-9d7e951c290ec5bbe6506e0ddb064808764bc636/terraform-modules.zip//ecs-service/v4"
-  service_name              = var.service_name
-  TAGGED_IMAGE              = var.TAGGED_IMAGE
-  enable_execute_command    = "true"
-  app_port                  = var.app_port
-  cpu_limit                 = var.cpu_limit
-  mem_reservation           = var.mem_reservation
-  mem_limit                 = var.mem_limit
-  app_env_vars              = local.app_env_vars
-  app_domain_name           = var.app_domain_name
-  ecs_wait_for_steady_state = true
-  tags                      = module.tags.result
+  source                           = "s3::https://s3-eu-central-1.amazonaws.com/terraform-modules-9d7e951c290ec5bbe6506e0ddb064808764bc636/terraform-modules.zip//ecs-service/v5"
+  service_name                     = var.service_name
+  TAGGED_IMAGE                     = var.TAGGED_IMAGE
+  enable_execute_command           = "true"
+  app_port                         = var.app_port
+  cpu_limit                        = var.cpu_limit
+  mem_reservation                  = var.mem_reservation
+  mem_limit                        = var.mem_limit
+  app_env_vars                     = local.app_env_vars
+  app_domain_name                  = var.app_domain_name
+  ecs_wait_for_steady_state        = true
+  ecs_service_update_timeout       = "10m"
+  container_restart_policy_enabled = var.container_restart_policy_enabled
+  tags                             = module.tags.result
   # Set load_balancer_configuration_enabled = true to enable ACM, load balancer resources creation
   load_balancer_configuration_enabled = true
 }
--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -28,6 +28,12 @@ variable "mem_reservation" {
   default = 64
 }
 
+variable "container_restart_policy_enabled" {
+  description = "Whether to enable restart policy for the container."
+  type        = bool
+  default     = true
+}
+
 variable "TAGGED_IMAGE" {
   type = string
 }
